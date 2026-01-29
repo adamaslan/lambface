d650735dd5a242ae98c16b1a11d6ea98a95d8776
adamaslan <chillcoders@gmal.com>
2026-01-28 21:52:12 -0500

    efficient1

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CanvasPanel from "./components/CanvasPanel";
@@ -8,22 +9,38 @@ import MixcloudScreen from "./components/screens/MixcloudScreen";
 import LeftBuildings from "./components/buildings/LeftBuildings";
 import RightBuildings from "./components/buildings/RightBuildings";
 
+/**
+ * The main page component.
+ *
+ * This component renders the main page of the web application,
+ * including the header, left and right buildings, the canvas panel,
+ * and the footer.
+ */
 export default function Page() {
   return (
-    <main className="min-h-screen relative bg-[#05050a]">
-      <Header />
+    <>
+      <Head>
+        <meta property="og:video" content="https://lambface.nuwrrrld.com/lamb2.mp4" />
+        <meta property="og:video:type" content="video/mp4" />
+        <meta property="og:video:width" content="1280" />
+        <meta property="og:video:height" content="720" />
+      </Head>
 
-      <LeftBuildings />
-      <RightBuildings />
+      <main className="min-h-screen relative bg-[#05050a]">
+        <Header />
 
-      <CanvasPanel>
-        <IdentityScreen />
-        <SoundCloudScreen />
-        <MixcloudScreen />
-        <ShowsScreen />
-      </CanvasPanel>
+        <LeftBuildings />
+        <RightBuildings />
 
-      <Footer />
-    </main>
+        <CanvasPanel>
+          <IdentityScreen />
+          <SoundCloudScreen />
+          <MixcloudScreen />
+          <ShowsScreen />
+        </CanvasPanel>
+
+        <Footer />
+      </main>
+    </>
   );
 }
